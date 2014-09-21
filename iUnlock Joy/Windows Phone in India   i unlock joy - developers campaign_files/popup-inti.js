2cd2221ed109e2e20c945ebe5587b3e0036ba32b
Shubhajit Saha <suvozit@live.com>
2014-09-21 09:54:22 +0530

    First Submit

---
+++ iUnlock Joy/Windows Phone in India   i unlock joy - developers campaign_files/popup-inti.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+	 
+			$(".various1").fancybox({
+				'titlePosition'		: 'inside',
+				'transitionIn'		: 'none',
+				'transitionOut'		: 'none'
+			});
+			
+			$(".various2").fancybox({
+				'titlePosition'		: 'inside',
+				'transitionIn'		: 'none',
+				'transitionOut'		: 'none'
+			});
+			
+			$(".various3").fancybox({
+									
+				'titlePosition'		: 'inside',
+				'transitionIn'		: 'none',
+				'transitionOut'		: 'none'
+			});
+
+			$(".various4").fancybox({
+									
+				'titlePosition'		: 'inside',
+				'transitionIn'		: 'none',
+				'transitionOut'		: 'none'
+			});
+			
+			$(".various5").fancybox({
+									
+				'titlePosition'		: 'inside',
+				'transitionIn'		: 'none',
+				'transitionOut'		: 'none'
+			});
+			$(".various6").fancybox({
+									
+				'titlePosition'		: 'inside',
+				'transitionIn'		: 'none',
+				'transitionOut'		: 'none'
+			});
+
+
+	  
+	 
+		});
---
+++ iUnlock Joy/Windows Phone in India   i unlock joy - developers campaign_files/scripts.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+ 
+	
+	var $scrollerWindow = $('#content2');
+	var $speed = 1000;
+	
+	 
+	function gotoHome()
+	{
+		$scrollerWindow.stop().scrollTo( $('#home'), $speed, {axis:'x',offset:{left: -20, top:0 }} );
+		
+		return false;
+	}
+	
+	function gotoHow()
+	{
+		$scrollerWindow.stop().scrollTo( $('#how'), $speed, {axis:'x',offset:{left: -20, top:0 }} );	 
+		return false;
+	}
+	
+	function gotoYouget()
+	{
+		$scrollerWindow.stop().scrollTo( $('#youget'), $speed, {axis:'x',offset:{left: -20, top:0 }} );	  
+		return false;
+	}
+	
+	function gotoRules()
+	{
+		$scrollerWindow.stop().scrollTo( $('#rules'), $speed, {axis:'x',offset:{left: -20, top:0 }} );	 
+		return false;
+	}
+	
+	function gotoUsers()
+	{
+		$scrollerWindow.stop().scrollTo( $('#users'), $speed, {axis:'x',offset:{left: -20, top:0 }} );	 
+		return false;
+	}
+	
+ 
+	
+	function noScroll()
+	{
+		return false;
+	}
+	
+	$('.goto_home').live("click", gotoHome);
+	$('.goto_how').live("click", gotoHow);
+	$('.goto_youget').live("click", gotoYouget);
+	$('.goto_rules').live("click", gotoRules);
+	$('.goto_users').live("click", gotoUsers);
+	 $('.noScroll').live("click", noScroll);
+ 
+	return false;
+	 
+		
+});
